a93dc48f4f2c5eb4648e34d4d47d452716e956c5
kimchunyong <lovemy58511@gmail.com>
2019-09-26 23:09:24 +0900

    [add] query

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,39 @@
+const people = [
+    {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    },
+    {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    }, {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    }, {
+        name: "Nicolas",
+        age: 16,
+        gender: "female"
+    }, {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    }, {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    }, {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    }
+]
+
 const resolvers = {
     Query: {
-        name: () => "nicolas"
+        people: () => people
     }
 };
 
